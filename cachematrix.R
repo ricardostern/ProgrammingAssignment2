24d3446b80b0d004c4d97ce33eecf273e3cd6cc4
ricardostern <ricardostern@hotmail.com>
2015-04-26 12:49:48 -0700

    Third week coding assignment - ristern
    Submitting for grading

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
+# John's Hopkins - R Programming Course (Coursera)
+# Week 3 Coding Assignment - caching the invert matrix of a matrix
+#
+# Note: This assignment used as starting point the examples given in 
+# the assignment page to cache the mean of a vector.
 
-## Write a short comment describing this function
+#
+# makeCacheMatrix 
+#
+# Factory method to create an enhanced *object* that caches the inverse 
+# of a matrix, by using R's lexical scoping.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix<- function(x = numeric()) 
+{
+    # Initialize cached inverse
+    inv <- NULL
 
+    # *set* member function
+    # initializes the object x and its inverse
+    set <- function(y) 
+    {
+        # initializes the value of x and inv in the parent scope
+        x <<- y
+        inv <<- NULL
+    }
+    
+    # *get* member function
+    # returns the value of the enhanced matrix
+    get <- function() 
+    {
+        x
+    }
+
+    # *getsolve* member function
+    # returns the inverse of a function - if cached, returns it from cache,
+    # otherwise executes solve. 
+    #
+    # Note: We are not passing in the additional optional argument of solve
+    # the inverse is only achieved when no other args are passed
+    getsolve <- function() 
+    {
+        if(is.null(inv)) 
+        {
+            # not previously cached, cache the value
+            inv <<- solve(get())
+        }   
+	  else
+        {
+            # tracing message only
+            print("getting cached data")
+        }
+
+        # always return it from cache
+        inv
+    }
+
+    # returns a list of all available member methods
+    list(set = set, 
+         get = get,
+         getsolve = getsolve)
 }
 
+#
+# cacheSolve
+#
+# returns the inverse of a function - if cached, returns it from cache,
+# otherwise executes solve.
+# 
+# Note: Instead of having the logic for checking and updating the cache in this
+# method we chose to add it to the getsolve() message, as it is closer to the data
+# it modifies and, hence, would yield better maintainability. In fact, if the 
+# assignment did not ask for a separate cacheSolve() function I would just have used
+# x$getsolve(.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) 
+{
+    x$getsolve()
 }
+
